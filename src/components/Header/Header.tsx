f2546b838b7bb04a04b00c6742d83f6388f73fd8
Artem Tyshchuk <artemtyschukk@gmail.com>
2023-12-28 13:34:32 +0100

    starter

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.scss";
+
+interface HeaderProps {}
+
+export const Header = ({}: HeaderProps) => {
+  return (
+    <div className={styles.header}>
+      <div></div>
+    </div>
+  );
+};
